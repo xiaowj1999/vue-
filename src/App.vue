50cbc3ed0575a323e96776f83117f5d008184183
xiaowj1999 <1262475768@qq.com>
2019-01-13 12:05:03 +0800

    登录静态布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app">
+  <div class="form-container">
+    <div class="login-content">
+      <h2>用户登录</h2>
+        <el-form label-position="top" label-width="80px" :model="formData">
+          <el-form-item label="用户名">
+              <el-input v-model="formData.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+              <el-input v-model="formData.password"></el-input>
+          </el-form-item>
+          <el-button type="success" class="el-login">登录</el-button>
+        </el-form>
+      </div>
   </div>
 </template>
 
@@ -7,8 +19,33 @@
 
 export default {
   name: 'app',
+  data(){
+    return {
+      formData:{
+        username:"",
+        password:""
+      }
+    }
+  }
 }
 </script>
 
 <style>
+  .form-container{
+    background-color:#324152;
+    height:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+  .login-content{
+    background-color:#ffffff;
+    width:580px;
+    padding:40px;
+    box-sizing:border-box;
+    border-radius:10px;
+  }
+  .el-login {
+    width:100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入我们使用的element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+//导入基本样式
+import "./assets/base.css"
+
 Vue.config.productionTip = false
 
 new Vue({
